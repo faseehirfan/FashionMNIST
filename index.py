29499dbcfab226b7b02d3295f398c4a0595de63c
Faseeh Irfan <faseeh_irfan@outlook.com>
2024-03-31 22:05:36 -0700

    set up layers of the model

--- index.py
+++ index.py
@@ -35,3 +35,21 @@ plt.imshow(image, cmap=plt.cm.binary)
 plt.colorbar()
 plt.grid(False)
 plt.show()
+
+# Set up layers of the model
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(40, (2, 2), padding='same', activation=tf.nn.relu, input_shape=(28, 28, 1)),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+
+    tf.keras.layers.Conv2D(60, (2, 2), padding='same', activation=tf.nn.relu),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+
+    tf.keras.layers.Conv2D(80, (2, 2), padding='same', activation=tf.nn.relu),
+    tf.keras.layers.MaxPooling2D((2, 2), strides=2),
+
+    tf.keras.layers.Flatten(),
+
+    tf.keras.layers.Dense(128, activation=tf.nn.relu),
+
+    tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
